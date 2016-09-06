9b18b2ff17f11658bffab59ded10b3a04cdc398f
Martin Appelmann <m.appelmann@tu-bs.de>
2016-09-06 23:24:21 +0200

    create login logout button

--- Dawn of the nuts/Assets/Scripts/Manager/APIManager.cs
+++ Dawn of the nuts/Assets/Scripts/Manager/APIManager.cs
@@ -4,19 +4,44 @@ using System.Collections;
 
 public class APIManager : MonoBehaviour {
 
-	
+	private bool isSignIn;
+	private string userName;
+	private Text userText;
 
-	void OnLevelWasLoaded(int level) {
-		Text userText = GameObject.Find("Username").GetComponent<Text>();
-		if (level == 1) GameJolt.UI.Manager.Instance.ShowSignIn();
+	void Awake() {
+		isSignIn = false;
+		userName = "you sign in as ";
+	}
 
-		GameJolt.UI.Manager.Instance.ShowSignIn((bool success) => {
-			if (success) {
-				userText.text = "You are sign in as " + GameJolt.API.Manager.Instance.CurrentUser.Name.ToString();
-				Debug.Log("The user " + GameJolt.API.Manager.Instance.CurrentUser.Name.ToString() + " signed in!");
-			} else {
-				Debug.Log("The user failed to signed in or closed the window :(");
-			}
-		});
+	//void OnLevelWasLoaded(int level) {
+	//	if (level == 1) {
+	//		userText = GameObject.Find("Username").GetComponent<Text>();
+	//		userText.text = userName;
+	//		if (!isSignIn) {
+	//			GameJolt.UI.Manager.Instance.ShowSignIn((bool success) => {
+	//				if (success) {
+	//					userName += GameJolt.API.Manager.Instance.CurrentUser.Name.ToString();
+	//					userText.text = userName;
+	//					isSignIn = true;
+	//					Debug.Log("The user " + GameJolt.API.Manager.Instance.CurrentUser.Name.ToString() + " signed in!");
+	//				} else {
+	//					Debug.Log("The user failed to signed in or closed the window :(");
+	//				}
+	//			});
+	//		}
+	//	}
+	//}
+
+	public void login() {
+		if (GameJolt.API.Manager.Instance.CurrentUser == null) {
+			GameJolt.UI.Manager.Instance.ShowSignIn();
+		}
+	}
+
+	public void logout() {
+		if (GameJolt.API.Manager.Instance.CurrentUser != null) {
+			GameJolt.API.Manager.Instance.CurrentUser.SignOut();
+		}
+
 	}
 }
