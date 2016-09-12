9b61f0af1d34a8f1e5c1a32e54b9730cef47b705
Martin Appelmann <m.appelmann@tu-bs.de>
2016-09-12 20:50:18 +0200

    improve login/logout
    in the webplayer you login as guest if you not login on gamejolt.

--- Dawn of the nuts/Assets/Scripts/Manager/APIManager.cs
+++ Dawn of the nuts/Assets/Scripts/Manager/APIManager.cs
@@ -4,43 +4,37 @@ using System.Collections;
 
 public class APIManager : MonoBehaviour {
 
-	private bool isSignIn;
-	private string userName;
 	private Text userText;
 
-	void Awake() {
-		isSignIn = false;
-		userName = "you sign in as ";
+	void OnLevelWasLoaded(int level) {
+		if (level == 1) {
+			userText = GameObject.Find("Username").GetComponent<Text>();
+			if (GameJolt.API.Manager.Instance.CurrentUser != null) {
+				userText.text = "you sign in as " + GameJolt.API.Manager.Instance.CurrentUser.Name.ToString();
+			} else {
+				userText.text = "You sign in as guest.";
+			}
+		}
 	}
-
-	//void OnLevelWasLoaded(int level) {
-	//	if (level == 1) {
-	//		userText = GameObject.Find("Username").GetComponent<Text>();
-	//		userText.text = userName;
-	//		if (!isSignIn) {
-	//			GameJolt.UI.Manager.Instance.ShowSignIn((bool success) => {
-	//				if (success) {
-	//					userName += GameJolt.API.Manager.Instance.CurrentUser.Name.ToString();
-	//					userText.text = userName;
-	//					isSignIn = true;
-	//					Debug.Log("The user " + GameJolt.API.Manager.Instance.CurrentUser.Name.ToString() + " signed in!");
-	//				} else {
-	//					Debug.Log("The user failed to signed in or closed the window :(");
-	//				}
-	//			});
-	//		}
-	//	}
-	//}
 
 	public void login() {
 		if (GameJolt.API.Manager.Instance.CurrentUser == null) {
-			GameJolt.UI.Manager.Instance.ShowSignIn();
+			GameJolt.UI.Manager.Instance.ShowSignIn((bool success) => {
+				if(success) {
+					userText = GameObject.Find("Username").GetComponent<Text>();
+					userText.text = "you sign in as " + GameJolt.API.Manager.Instance.CurrentUser.Name.ToString(); ;
+					Debug.Log("The user " + GameJolt.API.Manager.Instance.CurrentUser.Name.ToString() + " signed in!");
+				} 
+			});
 		}
 	}
 
 	public void logout() {
 		if (GameJolt.API.Manager.Instance.CurrentUser != null) {
 			GameJolt.API.Manager.Instance.CurrentUser.SignOut();
+			userText = GameObject.Find("Username").GetComponent<Text>();
+			userText.text = "you sign in as guest";
+			Debug.Log("logout");
 		}
 
 	}
