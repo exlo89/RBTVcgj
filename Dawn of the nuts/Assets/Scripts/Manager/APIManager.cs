d66202f951d767afbb1bbc5326a41cfeed23881f
Martin Appelmann <m.appelmann@tu-bs.de>
2016-09-12 23:30:50 +0200

    separate manager (backup)

--- Dawn of the nuts/Assets/Scripts/Manager/APIManager.cs
+++ Dawn of the nuts/Assets/Scripts/Manager/APIManager.cs
@@ -4,15 +4,32 @@ using System.Collections;
 
 public class APIManager : MonoBehaviour {
 
+	public GameObject inputName;
 	private Text userText;
+	private string guestName = "guest";
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) {
+			inputName.SetActive(true);
+			Debug.Log("Test");
+
+
+
+			//userText = GameObject.Find("Username").GetComponent<Text>();
+			//userText.text = "You sign in as " + guestName + " (guest) . Press enter for change the name.";
+		}
+	}
+
 
 	void OnLevelWasLoaded(int level) {
 		if (level == 1) {
 			userText = GameObject.Find("Username").GetComponent<Text>();
+			inputName.SetActive(false);
+			//inputName = GameObject.Find("GuestName");
 			if (GameJolt.API.Manager.Instance.CurrentUser != null) {
 				userText.text = "you sign in as " + GameJolt.API.Manager.Instance.CurrentUser.Name.ToString();
 			} else {
-				userText.text = "You sign in as guest.";
+				userText.text = "You sign in as guest. Press enter for change the name.";
 			}
 		}
 	}
@@ -33,9 +50,8 @@ public class APIManager : MonoBehaviour {
 		if (GameJolt.API.Manager.Instance.CurrentUser != null) {
 			GameJolt.API.Manager.Instance.CurrentUser.SignOut();
 			userText = GameObject.Find("Username").GetComponent<Text>();
-			userText.text = "you sign in as guest";
+			userText.text = "You sign in as guest. Press enter for change the name.";
 			Debug.Log("logout");
 		}
-
 	}
 }
